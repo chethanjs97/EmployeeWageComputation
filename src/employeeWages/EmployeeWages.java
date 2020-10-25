9a77dc8afade2c7ab56056f211fb7f0ae54f5f08
chethanjs97 <chethanjschethu@gmail.com>
2020-10-25 20:11:29 +0530

    added :Managing Employee Wages for Multiples companies

--- src/employeeWages/EmployeeWages.java
+++ src/employeeWages/EmployeeWages.java
@@ -2,59 +2,60 @@ package employeeWages;
 
 
 public class EmployeeWages {
-	
-		public static final int IS_FULL_TIME = 1;
-		public static final int IS_PART_TIME = 2;
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
 
-		private final int empRatePerHour;
-		private final int numOfWorkingDays;
-		private final int maxhoursPerMonth;
-		private final String company;
-		private int totalEmpWage;
-		public EmployeeWages(String company, int empRatePerHour, int numOfWorkingDays, int maxhoursPerMonth)
-		{
-			this.company=company;
-			this.empRatePerHour=empRatePerHour;
-			this.numOfWorkingDays=numOfWorkingDays;
-			this.maxhoursPerMonth=maxhoursPerMonth;
-		}
-		public void computeEmpWage() {
-			int emphrs = 0, totalworkingdays = 0, totalEmphrs = 0;
-			while (totalEmphrs < maxhoursPerMonth && totalworkingdays < numOfWorkingDays) {
-				totalworkingdays++;
-				int random = (int) Math.floor(Math.random() * 10) % 3;
-				switch (random) {
+	private final int empRatePerHour;
+	private final int numOfWorkingDays;
+	private final int maxhoursPerMonth;
+	private final String company;
+	private int totalEmpWage;
+	public EmployeeWages(String company, int empRatePerHour, int numOfWorkingDays, int maxhoursPerMonth)
+	{
+		this.company=company;
+		this.empRatePerHour=empRatePerHour;
+		this.numOfWorkingDays=numOfWorkingDays;
+		this.maxhoursPerMonth=maxhoursPerMonth;
+	}
+	public void computeEmpWage() {
+		int emphrs = 0, totalworkingdays = 0, totalEmphrs = 0;
+		while (totalEmphrs < maxhoursPerMonth && totalworkingdays < numOfWorkingDays) {
+			totalworkingdays++;
+			int random = (int) Math.floor(Math.random() * 10) % 3;
+			switch (random) {
 
-				case 1:
-					emphrs = 8;
-					break;
+			case 1:
+				emphrs = 8;
+				break;
 
-				case 2:
-					emphrs = 4;
-					break;
+			case 2:
+				emphrs = 4;
+				break;
 
-				default:
-					emphrs = 0;// not present
+			default:
+				emphrs = 0;// not present
 
-				}
-				totalEmphrs += emphrs;
-				System.out.println("days" +totalworkingdays+ "Emphrs" +emphrs);
 			}
-			totalEmpWage = totalEmphrs * empRatePerHour;
-			
+			totalEmphrs += emphrs;
+			System.out.println("days" +totalworkingdays+ "Emphrs" +emphrs);
 		}
-		//@override
-		public String toString(){
-		return "Total Emp wages for company " +company+ "is" +totalEmpWage;
-		}
+		totalEmpWage = totalEmphrs * empRatePerHour;
+		
+	}
+	//@override
+	public String toString(){
+	return "Total Emp wages for company " +company+ "is" +totalEmpWage;
+	}
 
-		public static void main(String[] args) {
-			EmployeeWages info = new EmployeeWages("infosys" ,20,2,10);
-			EmployeeWages cogni = new EmployeeWages("cognizath" ,40,4,20);
-			info.computeEmpWage();
-			System.out.println(info);
-			cogni.computeEmpWage();
-			System.out.println(cogni);
-			
+	public static void main(String[] args) {
+		EmployeeWages info = new EmployeeWages("infosys" ,20,2,10);
+		EmployeeWages cogni = new EmployeeWages("cognizath" ,40,4,20);
+		info.computeEmpWage();
+		System.out.println(info);
+		cogni.computeEmpWage();
+		System.out.println(cogni);
+		
+	}
+
+	
 		}
-}
